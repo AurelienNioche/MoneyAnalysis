180f5842061c8e769feb7f400e5f71c55da7d3c0
AurelienNioche <nioche.aurelien@gmail.com>
2018-07-02 19:59:33 +0200

    update

---
+++ analysis/monetary_behavior.py
@@ -0,0 +1,12 @@
+# for agent in self.agents:
+#
+#     agent_choice = agent.which_exchange_do_you_want_to_try()
+#     self.markets[agent_choice].append(agent.idx)
+#
+#     if self.m in (agent.P, agent.C):
+#         monetary_conform = agent_choice == (agent.P, agent.C)
+#
+#     else:
+#         monetary_conform = agent_choice in [(agent.P, self.m), (self.m, agent.C)]
+#
+#     self.bkp_monetary_bhv[agent.idx, self.t] = int(monetary_conform)
