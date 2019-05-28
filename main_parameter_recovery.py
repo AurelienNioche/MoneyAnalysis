f810cd9243f6bbeb65a25884388442e570a7b9f6
Aurélien Nioche <nioche.aurelien@gmail.com>
2019-05-28 12:59:39 +0300

    BIG UPDATE

--- main_parameter_recovery.py
+++ main_parameter_recovery.py
@@ -1,21 +1,7 @@
-import os
-import numpy as np
-
-from simulation.model.RL.rl_agent import RLAgent
-
-from backup import backup, structure
-
 import matplotlib.pyplot as plt
 
 from fit import fit
 from xp import xp
-
-from simulation.run import _run
-
-from metric import metric
-
-import graph.sim_and_xp
-from stats import stats
 
 import fit.data
 import simulation.run_based_on_fit
@@ -46,42 +32,6 @@ def main():
 
     plt.tight_layout()
     plt.savefig("fig/parameter_recovery.pdf")
-    # category = data.keys()
-    # n_good_cond = np.unique(room_n_good)
-    # cond_labels = "NON-UNIF", "UNIF"
-    #
-    # fig_data = {n_good: {
-    #     cat: {
-    #
-    #     } for cat in category
-    # } for n_good in n_good_cond}
-    #
-    # for n_good in room_n_good:
-    #
-    #     for uniform in True, False:
-    #
-    #         # Find the good indexes
-    #         cond_n_good = room_n_good == n_good
-    #         cond_uniform = room_uniform == uniform
-    #
-    #         xp_cond = cond_n_good * cond_uniform
-    #         assert (np.sum(xp_cond) == 1)
-    #         d_idx = np.where(xp_cond == 1)[0][0]
-    #
-    #         for cat in category:
-    #
-    #             # Get formatted data
-    #             d = data[cat][d_idx]
-    #             d_formatted = metric.dynamic_data(data_xp_session=d)
-    #
-    #             for agent_type in sorted(d_formatted.keys()):
-    #                 if agent_type not in fig_data[n_good][cat].keys():
-    #                     fig_data[n_good][cat][agent_type] = {}
-    #
-    #                 fig_data[n_good][cat][agent_type][cond_labels[int(uniform)]] = d_formatted[agent_type]
-    #
-    # graph.sim_and_xp.plot(fig_data, name_extension='FIT')
-    # stats.sim_and_xp(fig_data)
 
 
 if __name__ == "__main__":
